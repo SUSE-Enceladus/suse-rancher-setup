e6f8a72be5c7a42625dd4ff8aa733ffc255056a0
James Mason <jmason@suse.com>
2022-02-16 10:57:34 -0800

    Bootstrap v4

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery", to: "jquery.slim.min.js", preload: true # @3.6.0
+pin "bootstrap", to: "bootstrap.bundle.min.js", preload: true # @4.6.1
