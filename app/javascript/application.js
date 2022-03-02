5498d0aa0b4002a65f272c19f7d653bbec1ec8a7
James Mason <jmason@suse.com>
2022-03-02 09:55:05 -0800

    Remove hotwire support (conflicting with EOS)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "trix"
+import "@rails/actiontext"
 import "jquery"
 import "bootstrap"
 import "eos-ds"
-import "trix"
-import "@rails/actiontext"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jquery", to: "jquery.min.js" # @3.6.0
 pin "bootstrap", to: "bootstrap.bundle.min.js" # @4.6.1
