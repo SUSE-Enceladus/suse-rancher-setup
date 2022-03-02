b2525bd661033c92fff6b9cc841d3fcb011d6c97
James Mason <jmason@suse.com>
2022-03-02 07:27:15 -0800

    EOS requires full jQuery

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "jquery", to: "jquery.slim.min.js" # @3.6.0
+pin "jquery", to: "jquery.min.js" # @3.6.0
 pin "bootstrap", to: "bootstrap.bundle.min.js" # @4.6.1
 pin "eos-ds", to: "eos-ds.js"
 pin "trix"
