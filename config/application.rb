39a35c1fdc887cb35f7b49f4a1feeb94db800393
James Mason <jmason@suse.com>
2022-03-15 22:17:16 -0700

    Enable a background job scheduler

--- config/application.rb
+++ config/application.rb
@@ -11,15 +11,13 @@ module CuratedCloudInstaller
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Asynchronous in-memory background job handler
+    config.active_job.queue_adapter = :async
 
+    # Engines to load
     config.engines = [ 'ShirtSize', 'Aws', 'Helm', 'RancherOnEks']
+
+    # Application-level menu entries
     config.menu_entries = [
       { caption: 'Welcome', icon: 'home', target: '/'}
     ]
