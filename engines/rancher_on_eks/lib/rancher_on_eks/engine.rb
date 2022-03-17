14d6bf18769cd3075946e9aca1b5fd26430fc8ff
James Mason <jmason@suse.com>
2022-03-17 13:13:19 -0700

    Cleanup unused code

--- engines/rancher_on_eks/lib/rancher_on_eks/engine.rb
+++ engines/rancher_on_eks/lib/rancher_on_eks/engine.rb
@@ -1,9 +1,5 @@
 module RancherOnEks
   class Engine < ::Rails::Engine
     isolate_namespace RancherOnEks
-
-    # config.before_initialize do
-    #   config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
-    # end
   end
 end
