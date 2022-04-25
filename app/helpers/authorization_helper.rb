25a2dbaa46ebcc1ccbc6328c9d3a5fda08782c0c
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2022-04-25 19:19:11 +0100

    Add navigation order
    This Fixes #22

---
+++ app/helpers/authorization_helper.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+# Authorize access to steps
+module AuthorizationHelper
+  def can(path)
+    result = flow_restriction_checks(path)
+    logger.debug "AUTH: #{result ? 'can' : 'cannot'} access #{path}"
+    result
+  end
+
+  def flow_restriction_checks(path)
+    case path
+    when  "/"
+      true
+    when AWS::Engine.routes.url_helpers.edit_credential_path
+      true
+    when AWS::Engine.routes.url_helpers.edit_region_path
+      credentials_set?
+    when ShirtSize::Engine.routes.url_helpers.edit_size_path
+      true
+    when RancherOnEks::Engine.routes.url_helpers.edit_fqdn_path
+      credentials_set? && region_set?
+    when RancherOnEks::Engine.routes.url_helpers.steps_path
+      fqdn_set?
+    else
+      false
+    end
+  end
+
+  def credentials_set?
+    credentials = AWS::Credential.load()
+    credentials.aws_access_key_id.present? && credentials.aws_secret_access_key.present?
+  end
+
+  def region_set?
+    region = AWS::Region.load
+    region.value.present?
+  end
+
+  def fqdn_set?
+    fqdn = RancherOnEks::Fqdn.load
+    fqdn.value.present?
+  end
+end
