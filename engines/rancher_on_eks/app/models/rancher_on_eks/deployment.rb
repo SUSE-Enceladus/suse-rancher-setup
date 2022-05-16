0efdd6384d6306584d5a53e81548fe8962901700
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2022-05-16 23:55:39 +0100

    Add Cluster and rest of steps

--- engines/rancher_on_eks/app/models/rancher_on_eks/deployment.rb
+++ engines/rancher_on_eks/app/models/rancher_on_eks/deployment.rb
@@ -55,65 +55,65 @@ module RancherOnEks
         action: 'Create a NAT Gateway for private subnets on Elastic IP address'
       )
 
-      # index = 1
-      # (12..14).each do |rank|
-      #   Step.create!(
-      #     rank: rank,
-      #     duration: 14,
-      #     action: "Route private subnet #{index}/3 through NAT Gateway"
-      #   )
-      #   index += 1
-      # end
-      # Step.create!(
-      #   rank: 15,
-      #   duration: 1,
-      #   action: "Create a Security Group for the EKS control plane"
-      # )
-      # Step.create!(
-      #   rank: 16,
-      #   duration: 14,
-      #   action: "Define an IAM Role for the EKS control plane"
-      # )
-      # Step.create!(
-      #   rank: 17,
-      #   duration: 518,
-      #   action: 'Create the EKS control plane'
-      # )
-      # Step.create!(
-      #   rank: 18,
-      #   duration: 16,
-      #   action: "Define an IAM Role for the EKS worker nodes"
-      # )
-      # Step.create!(
-      #   rank: 19,
-      #   duration: 156,
-      #   action: 'Create a worker node group in the EKS cluster'
-      # )
-      # Step.create!(
-      #   rank: 20,
-      #   duration: 1,
-      #   action: 'Fetch the kubeconfig for the EKS cluster'
-      # )
-      # Step.create!(
-      #   rank: 21,
-      #   duration: 35,
-      #   action: 'Deploy Ingress controller'
-      # )
-      # Step.create!(
-      #   rank: 22,
-      #   duration: 32,
-      #   action: 'Add DNS entry for Ingress controller'
-      # )
-      # Step.create!(
-      #   rank: 23,
-      #   duration: 50,
-      #   action: 'Deploy Certificate manager'
-      # )
-      # Step.create!(
-      #   rank: 24,
-      #   duration: 18,
-      #   action: 'Deploy Rancher'
-      # )
+      index = 1
+      (12..14).each do |rank|
+        Step.create!(
+          rank: rank,
+          duration: 14,
+          action: "Route private subnet #{index}/3 through NAT Gateway"
+        )
+        index += 1
+      end
+      Step.create!(
+        rank: 15,
+        duration: 1,
+        action: "Create a Security Group for the EKS control plane"
+      )
+      Step.create!(
+        rank: 16,
+        duration: 14,
+        action: "Define an IAM Role for the EKS control plane"
+      )
+      Step.create!(
+        rank: 17,
+        duration: 518,
+        action: 'Create the EKS control plane'
+      )
+      Step.create!(
+        rank: 18,
+        duration: 16,
+        action: "Define an IAM Role for the EKS worker nodes"
+      )
+      Step.create!(
+        rank: 19,
+        duration: 156,
+        action: 'Create a worker node group in the EKS cluster'
+      )
+      Step.create!(
+        rank: 20,
+        duration: 1,
+        action: 'Fetch the kubeconfig for the EKS cluster'
+      )
+      Step.create!(
+        rank: 21,
+        duration: 35,
+        action: 'Deploy Ingress controller'
+      )
+      Step.create!(
+        rank: 22,
+        duration: 32,
+        action: 'Add DNS entry for Ingress controller'
+      )
+      Step.create!(
+        rank: 23,
+        duration: 50,
+        action: 'Deploy Certificate manager'
+      )
+      Step.create!(
+        rank: 24,
+        duration: 18,
+        action: 'Deploy Rancher'
+      )
     end
 
     def step(rank, force: false)
@@ -214,84 +214,81 @@ module RancherOnEks
         )
         @nat.wait_until(:available)
       end
-      # @private_route_tables = []
-      # index = 0
-      # (12..14).each do |rank|
-      #   step(rank) do
-      #     private_subnet = @private_subnets[index]
-      #     private_route_table = AWS::RouteTable.create(vpc_id: @vpc.id, name: "private-route-table-#{index}")
-      #     private_subnet.set_route_table!(private_route_table.id)
-      #     @private_route_tables << private_route_table
-      #     index += 1
-      #     private_route_table.wait_until(:available)
-      #   end
-      # end
-      # step(15) do
-      #   @security_group = AWS::SecurityGroup.create(vpc_id: @vpc.id)
-      # end
-      # step(16) do
-      #   @cluster_role = AWS::Role.create(target: 'cluster')
-      # end
+      @private_route_tables = []
+      index = 0
+      (12..14).each do |rank|
+        step(rank) do
+          private_subnet = @private_subnets[index]
+          private_route_table = AWS::RouteTable.create(vpc_id: @vpc.id, name: "private-route-table-#{index}")
+          private_subnet.set_route_table!(private_route_table.id)
+          @private_route_tables << private_route_table
+          index += 1
+          private_route_table.wait_until(:available)
+        end
+      end
+      step(15) do
+        @security_group = AWS::SecurityGroup.create(vpc_id: @vpc.id)
+      end
+      step(16) do
+        @cluster_role = AWS::Role.create(target: 'cluster')
+      end
 
-      # step(17) do
-      #   puts "STEP 17"
-      #   subnet_ids =
-      #     @public_subnets.collect(&:id) + @private_subnets.collect(&:id)
-      #   @cluster = AWS::Cluster.create(
-      #     sg_id: @security_group.id,
-      #     role_arn: @cluster_role.arn,
-      #     subnet_ids: subnet_ids
-      #   )
-      #   @cluster.wait_until(:ACTIVE)
-      # end
-      # step(18) do
-      #   @ng_role = AWS::Role.create(target: 'nodegroup')
-      # end
-      # step(19) do
-      #   @public_subnet_ids = @public_subnets.collect(&:id)
-      #   @nodegroup = AWS::NodeGroup.create(
-      #     cluster_name: @cluster.id,
-      #     role_arn: @ng_role.arn,
-      #     subnet_ids: @public_subnet_ids,
-      #     instance_type: @cluster_size.instance_type,
-      #     instance_count: @cluster_size.instance_count
-      #   )
-      #   @nodegroup.wait_until(:ACTIVE)
-      # end
-      # step(20) do
-      #   @cli.update_kube_config(@cluster.id)
-      #   nil
-      # end
-      # step(21) do
-      #   @ingress = Helm::IngressController.create()
-      #   @ingress.wait_until(:deployed)
-      # end
-      # step(22) do
-      #   @ingress ||= Step.find_by_rank(21).resource
-      #   @fqdn_record = AWS::DnsRecord.create(
-      #     fqdn: @fqdn.value,
-      #     target: @ingress.hostname(),
-      #     record_type: 'CNAME'
-      #   )
-      #   # @fqdn_record.wait_until(:available)
-      #   # wait loop in DnsRecord is broken FIXME
-      # end
-      # step(23) do
-      #   @cert_manager = Helm::CertManager.create()
-      #   @cert_manager.wait_until(:deployed)
-      # end
-      # step(24) do
-      #   @fqdn_record ||= Step.find_by_rank(22).resource
-      #   @rancher = RancherOnEks::Rancher.create(fqdn: @fqdn_record.id)
-      #   @rancher.wait_until(:deployed)
-      # end
+      step(17) do
+        puts "STEP 17"
+        subnet_ids =
+          @public_subnets.collect(&:id) + @private_subnets.collect(&:id)
+        @cluster = AWS::Cluster.create(
+          sg_id: @security_group.id,
+          role_arn: @cluster_role.arn,
+          subnet_ids: subnet_ids
+        )
+        @cluster.wait_until(:ACTIVE)
+      end
+      step(18) do
+        @ng_role = AWS::Role.create(target: 'nodegroup')
+      end
+      step(19) do
+        @public_subnet_ids = @public_subnets.collect(&:id)
+        @nodegroup = AWS::NodeGroup.create(
+          cluster_name: @cluster.id,
+          role_arn: @ng_role.arn,
+          subnet_ids: @public_subnet_ids,
+          instance_type: @cluster_size.instance_type,
+          instance_count: @cluster_size.instance_count
+        )
+        @nodegroup.wait_until(:ACTIVE)
+      end
+      step(20) do
+        @cli.update_kube_config(@cluster.id)
+        nil
+      end
+      step(21) do
+        @ingress = Helm::IngressController.create()
+        @ingress.wait_until(:deployed)
+      end
+      step(22) do
+        @ingress ||= Step.find_by_rank(21).resource
+        @fqdn_record = AWS::DnsRecord.create(
+          fqdn: @fqdn.value,
+          target: @ingress.hostname(),
+          record_type: 'CNAME'
+        )
+        # @fqdn_record.wait_until(:available)
+        # wait loop in DnsRecord is broken FIXME
+      end
+      step(23) do
+        @cert_manager = Helm::CertManager.create()
+        @cert_manager.wait_until(:deployed)
+      end
+      step(24) do
+        @fqdn_record ||= Step.find_by_rank(22).resource
+        @rancher = RancherOnEks::Rancher.create(fqdn: @fqdn_record.id)
+        @rancher.wait_until(:deployed)
+      end
     end
 
     def self.rollback
       Step.all.order(rank: :desc).each do |step|
-        puts "Step rank rolling back #{step.rank}"
-        # next if [18].include? step.rank
-        puts step.rank
         step.resource&.destroy
         step.destroy if Rails.application.config.lasso_run.present?
       end
