228e7bca894343a84b06bdf0c46486a4cedc36d6
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2022-05-18 10:08:53 +0100

    Reuse method result

--- engines/aws/app/controllers/aws/regions_controller.rb
+++ engines/aws/app/controllers/aws/regions_controller.rb
@@ -6,12 +6,13 @@ module AWS
 
     def update
       @region = Region.new(region_params)
-      if @region.supported_instance_types.present? && @region.save
+      supported_instances = @region.supported_instance_types
+      if supported_instances.present? && @region.save
         flash[:success] = t('engines.aws.region.using', region: @region.value)
         redirect_to(helpers.next_step_path(aws.edit_region_path))
       else
-        flash[:warning] = @region.errors.full_messages if @region.supported_instance_type
-        flash[:danger] = t('engines.aws.region.not_available', region: @region.value) unless @region.supported_instance_type
+        flash[:warning] = @region.errors.full_messages if supported_instances
+        flash[:danger] = t('engines.aws.region.not_available', region: @region.value) unless supported_instances
 
         redirect_to(aws.edit_region_path)
       end
