438dca549bed5e27fc4cf8c5dc7516564d50f042
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2022-06-16 17:22:00 +0100

    Update config
    - Add new route
    - Add nginx path
    - Add logged flag

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :rancher_setup_logins, only: [:index, :update]
   # Defines the root path route ("/")
-  root "welcome#greeting"
+  root "rancher_setup_logins#index"
+  get "/welcome", to: "welcome#greeting"
 
   # 1 Deployable solution should be mounted
   mount RancherOnEks::Engine, at: '/deploy' if defined?(RancherOnEks::Engine)
