c51d80a0e70a69370a014266f5039fbf0a9f34c2
James Mason <jmason@suse.com>
2022-08-16 12:57:16 -0700

    Debug logging of translation keys

--- config/initializers/i18n_debug.rb
+++ config/initializers/i18n_debug.rb
@@ -2,7 +2,7 @@ require 'i18n'
 
 module I18nLogging
   def translate(locale, key, options = EMPTY_HASH)
-    puts "Translate: #{locale}: #{key}"
+    Rails.logger.debug("Translate: #{locale}: #{key}")
     super
   end
 end
