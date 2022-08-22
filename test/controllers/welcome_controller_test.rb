5ed5fdd4c8f7487d5afaede762afe0bb1665ef6c
James Mason <jmason@suse.com>
2022-08-22 10:56:42 -0700

    Fix up existing tests

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,18 @@
 require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    mock_login
+  end
+
   test "should get greeting" do
-    get welcome_greeting_url
+
+    mock_login
+    get welcome_url
     assert_response :success
+  end
+
+  teardown do
+    clear_login
   end
 end
