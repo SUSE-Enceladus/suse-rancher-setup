d5b83c14a28d114c012741ae516ec7b7aa7c048a
James Mason <jmason@suse.com>
2022-11-29 09:05:48 -0800

    Remove (unused) test dirs from engines

--- engines/helm/test/helm_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class HelmTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert Helm::VERSION
-  end
-end
--- engines/shirt_size/test/shirt_size_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ShirtSizeTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert ShirtSize::VERSION
-  end
-end
