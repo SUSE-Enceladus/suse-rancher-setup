27be14babee61499a6824c9bee1432b6cdeced2c
James Mason <jmason@suse.com>
2022-11-30 17:28:50 -0800

    Add steps for AKS deployment

--- engines/rancher_on_aks/app/models/rancher_on_aks/deployment.rb
+++ engines/rancher_on_aks/app/models/rancher_on_aks/deployment.rb
@@ -6,6 +6,46 @@ module RancherOnAks
         duration: 1,
         action: 'Prep'
       )
+      Step.create!(
+        rank: 1,
+        duration: 1,
+        action: 'Create a resource group'
+      )
+      Step.create!(
+        rank: 2,
+        duration: 1,
+        action: 'Create an AKS cluster'
+      )
+      Step.create!(
+        rank: 3,
+        duration: 1,
+        action: 'Fetch the kubeconfig for the AKS cluster'
+      )
+      Step.create!(
+        rank: 4,
+        duration: 1,
+        action: 'Deploy the ingress controller'
+      )
+      Step.create!(
+        rank: 5,
+        duration: 1,
+        action: 'Find the IP address of the load balancer'
+      )
+      Step.create!(
+        rank: 6,
+        duration: 1,
+        action: 'Create a DNS record for the Rancher server'
+      )
+      Step.create!(
+        rank: 7,
+        duration: 1,
+        action: 'Deploy the certificate manager'
+      )
+      Step.create!(
+        rank: 8,
+        duration: 1,
+        action: 'Deploy Rancher'
+      )
     end
 
     def deploy()
