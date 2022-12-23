e564c72737b0cbd82515ac9fb6015a82063f0d02
James Mason <jmason@suse.com>
2022-12-22 21:27:33 -0800

    Enable ShirtSize engine in Azure workflow
    Also add the missing Helm engine to the example config

--- engines/rancher_on_aks/app/helpers/rancher_on_aks/authorization_helper.rb
+++ engines/rancher_on_aks/app/helpers/rancher_on_aks/authorization_helper.rb
@@ -16,6 +16,8 @@ module RancherOnAks
         valid_login?
       when azure.edit_region_path, azure.region_path
         valid_login?
+      when shirt_size.edit_size_path, shirt_size.size_path
+        valid_login? && region_set?
       when rancher_on_aks.edit_fqdn_path, rancher_on_aks.fqdn_path
         valid_login? && region_set?
       when rancher_on_aks.steps_path, rancher_on_aks.deploy_steps_path
